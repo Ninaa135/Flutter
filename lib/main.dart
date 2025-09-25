7edd4c4e39c22057e37a878d7109efd03c84d7bb
Ninaa135 <nina.lp3i.krw24.com>
2025-09-25 21:35:18 +0700

    quiz

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:latihanflutter/latihan/pages/page_1.dart';
 import 'package:latihanflutter/latihan/pages/page_2.dart';
+import 'package:latihanflutter/tugas/quiz.dart';
 
 
 void main() {
@@ -13,12 +14,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      routes: {
-        "/" :(context)=>Page1(),
-        "/halaman2":(context)=>Page2()
-      },
-      initialRoute: "/",
-      // home: Page1()
+      // routes: {
+      //   "/" :(context)=>Page1(),
+      //   "/halaman2":(context)=>Page2()
+      // },
+      // initialRoute: "/",
+       home: Quiz()
       );
   }
 }
---
+++ lib/tugas/quiz.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class Quiz extends StatelessWidget {
+  const Quiz({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Text("Training",
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+            ),
+            ),
+          Text("Your Program",
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w500
+            ),
+            ),
+          Container(
+            width: 400,
+            height: 200,
+            decoration: BoxDecoration(
+              color: Colors.purpleAccent,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20),
+                topRight: Radius.circular(150),
+                bottomLeft: Radius.circular(20),
+                bottomRight: Radius.circular(20))
+            ),            
+          ),
+        Align(alignment: Alignment(0, 0),
+        child: Image.asset("gambar/card.jpg",
+        height: 200,
+        width: 400,
+        fit: BoxFit.contain,
+        ),
+        ),
+        ],
+      ),
+    );
+  }
+}
